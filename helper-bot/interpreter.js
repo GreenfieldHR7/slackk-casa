1d15068cbe5dab0993ee88f8c5d4abb77825ae16
Sam Martin <samuelthomasmartin@gmail.com>
2018-01-17 11:59:07 -0800

    draft of message filtering functionality in interpreter - need to debug

--- helper-bot/interpreter.js
+++ helper-bot/interpreter.js
@@ -3,20 +3,75 @@
 //const tasks = require('./tasks');
 
 const interpreter = (text, username, workspaceId) => {
+  let words = text.split(' ');
+  let errorMessage = '';
+  
   if (text.indexOf('remind') > -1) {
-  	console.log('reminder');
-  } else if (text.indexOf('news') > -1) {
-  	console.log('news');
+  	let task = undefined;
+  	let taskIndex = undefined;
+  	let timeToRemind = undefined;
+  	let timeToRemindIndex = undefined;
+
+  	words.forEach((word, index) => {
+  	  let lowerCaseWord = word.toLowerCase();
+  	  
+  	  if (lowerCaseWord === 'to') {
+  	  	taskIndex = index;
+  	  }
+
+	  if (lowerCaseWord === 'tomorrow' || lowerCaseWord === 'next' || lowerCaseWord === 'later' || lowerCaseWord === 'in' || lowerCaseWord === 'on') {
+  	  	if (timeToRemindIndex === undefined) {
+  	  	  timeToRemindIndex = index;	
+  	  	}
+  	  }  	  
+  	});
+
+  	if (taskIndex && timeToRemindIndex) {
+  	  task = words.slice(taskIndex + 1, timeToRemindIndex).join(' ');
+  	  timeToRemind = words.slice(timeToRemindIndex).join(' ');
+	  console.log(task);
+	  console.log(timeToRemind);
+  	} else {
+  	  errorMessage = 'Hmm. I didn\'t quite get that. Try something like "/helper-bot remind me to take out the trash tomorrow at 5pm"';
+  	  console.log(errorMessage);
+  	  //invoke error handler
+  	}
+  } else if (text.indexOf('news') > -1 || text.indexOf('stories') > -1) {
+  	let term = undefined;
+
+  	words.forEach((word, index) => {
+  	  let lowerCaseWord = word.toLowerCase();
+  	  
+  	  if (lowerCaseWord === 'on' || lowerCaseWord === 'about') {
+  	  	term = words.slice(index + 1).join(' ');
+	  	//invoke tasks.newsfetcher(term);
+  	  } else {
+  	  	errorMessage = 'Hmm. I didn\'t quite get that. Try something like "/helper-bot send me news about cats"';
+  	    console.log(errorMessage);
+  	  	//invoke error handler
+  	  }
+  	});
   } else {
-  	console.log('error');
+  	errorMessage = 'Hmm. I didn\'t quite get that. I can help with stuff like setting up reminders and fetching the latest news';
+  	console.log(errorMessage);
+  	//invoke error handler
   }
 }
 
 
 module.exports = { interpreter };
 
-//remind me to take out the trash tomorrow at 5pm
+// /helper-bot remind me to take out the trash tomorrow at 5pm
+// /helper-bot remind me to pack later today
+// /helper-bot remind me to pack later next week
+// /helper-bot remind me to brush my teeth in 60 seconds
+// /helper-bot remind me to take out the trash on Sunday
+// /helper-bot remind aaaaahhhhhhh!
 
-//send me news
-//send me news about cats
-//send me news on cats
+// /helper-bot send me stories about cats
+// /helper-bot send me news on cats
+// /helper-bot send me stories about cats
+// /helper-bot send aaaahhh!
+
+// /helper-bot aaaahhhh!
+
