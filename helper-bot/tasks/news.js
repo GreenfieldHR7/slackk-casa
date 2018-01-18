f8b2fa8104ab4b108d612abd609185cab7fc279f
Sam Martin <samuelthomasmartin@gmail.com>
2018-01-17 16:36:13 -0800

    implement newsFetcher with term

--- helper-bot/tasks/news.js
+++ helper-bot/tasks/news.js
@@ -6,7 +6,31 @@ const config = require('../../config.js');
 
 const newsFetcher = (term, workspaceId) => {
   if (term) {
-  	//
+  	let options = { 
+  	  method: 'GET',
+      url: 'https://newsapi.org/v2/everything',
+      qs: { 
+      	q: term,
+      	sortBy: 'popularity',
+      	apiKey: config.googleNewsAPI_Token
+      },
+      json: true
+    };
+
+    request(options, (error, response, body) => {
+      if (error) {
+      	console.error(error);
+      } else {
+        let news = [];
+
+        for (let i = 0; i < 5; i++) {
+          let article = body.articles[i];
+          news.push(article.title);
+        }
+
+        console.log(news);
+      }
+    });
   } else {
   	let options = { 
   	  method: 'GET',
@@ -14,18 +38,18 @@ const newsFetcher = (term, workspaceId) => {
       qs: { 
       	country: 'us', 
       	apiKey: config.googleNewsAPI_Token
-      }
+      },
+      json: true
     };
 
     request(options, (error, response, body) => {
       if (error) {
       	console.error(error);
       } else {
-        let res = JSON.parse(body);
         let news = [];
 
         for (let i = 0; i < 5; i++) {
-          let article = res.articles[i];
+          let article = body.articles[i];
           news.push(article.title);
         }
 
