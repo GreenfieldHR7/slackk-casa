f830351e1fcabfae34b789b164e3c896f32e8896
Sam Martin <samuelthomasmartin@gmail.com>
2018-01-18 10:07:19 -0800

    refactor responder func

--- helper-bot/responder.js
+++ helper-bot/responder.js
@@ -10,29 +10,35 @@ const response = (code, message, method, data) =>
     data,
   });
 
-const responder = async (task, workspaceId, message, ws, wss) => {
-  if (task === 'news') {
-    let postedMessage = await db.postMessage(
-      message,
-      'helper-bot',
-      workspaceId,
-    );
-  
-    [postedMessage] = postedMessage.rows;
-    ws.send(response(201, 'Post success', 'POSTMESSAGE', postedMessage));
+const sendBotMessage = async (task, workspaceId, message, ws, wss) => {
+  let postedMessage = await db.postMessage(
+    message,
+    'helper-bot',
+    workspaceId,
+  );
 
-  	socket.updateEveryoneElse(
-  	  ws, 
-  	  wss, 
-  	  response(200, 'New message', 'NEWMESSAGE', {
-        message: postedMessage,
-        workspaceId: workspaceId,
-      })
-    );
+  [postedMessage] = postedMessage.rows;
+  ws.send(response(201, 'Post success', 'POSTMESSAGE', postedMessage));
+
+	socket.updateEveryoneElse(
+	  ws, 
+	  wss, 
+	  response(200, 'New message', 'NEWMESSAGE', {
+      message: postedMessage,
+      workspaceId: workspaceId,
+    })
+  );  
+}
+
+const responder = (task, workspaceId, message, ws, wss) => {
+  if (task === 'news') {
+    sendBotMessage(task, workspaceId, message, ws, wss);
   } else if (task === 'notes') {
   	//
   } else if (task === 'reminders') {
   	//
+  } else {
+    //error case
   }
 }
 
--- helper-bot/tasks/reminders.js
+++ helper-bot/tasks/reminders.js
@@ -1,9 +1,21 @@
-//import stuff
+const output = require('../../helper-bot/responder.js');
+
 
 const reminderMaker = (task, time, workspaceId) => {
   console.log(task);
   console.log(time);
+
+  //reminder = 'I will remind you to x in y'
+  //output.responder('reminders', workspaceId, reminder, ws, wss);
 };
 
 
 module.exports = {reminderMaker};
+
+// VALID INPUT FORMATS
+// /helper-bot remind me to take out the trash tomorrow at 5pm
+// /helper-bot remind me to pack later today
+// /helper-bot remind me to pack later next week
+// /helper-bot remind me to brush my teeth in 60 seconds
+// /helper-bot remind me to take out the trash on Sunday
+// /helper-bot remind aaaaahhhhhhh!
