9db0887206f61797f7ccc4bd28dc26bd77d61ebd
Ofir Zisman <zismanofir@gmail.com>
2018-01-17 19:04:12 -0800

    Clean input text box after search, end add refresh button

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -1,13 +1,40 @@
 import React from 'react';
 
-export default ({ getMessagesByKeywords, handleSelectedUser }) => {
-	let query = '';
+export default class Search extends React.Component {
+  constructor(props) {
+	super(props);
+	this.state = {
+		query: ''
+	}
+  }
+
+  searchMessageByKeywords(query) {
+  	this.props.getMessagesByKeywords(query);
+  	this.setState( { query: '' } );
+  }
+
+  handleChangeInInput(e) {
+  	this.props.handleSelectedUser();
+  	if (e) {
+	  	this.setState( { query: e.target.value });  		
+  	}
+  }
+	
+  render() {
+	let { 
+		getMessagesByKeywords, 
+		handleSelectedUser 
+	} = this.props;
+	let query = this.state.query;
 	return (
-		<form className="example" action="action_page.php">
-			<input type='text' id='link_id' ref={input => query = input}
-				onChange={(e) => handleSelectedUser()}></input>
-			<input type='button' id='link' value='Search' 
-				onClick={(e) => getMessagesByKeywords(query)}></input>
+		<form>
+			<input type='text' value={query} ref={input => query = input}
+				onChange={(e) => this.handleChangeInInput(e)}></input>
+			<input type='button' value='Search' 
+				onClick={(e) => this.searchMessageByKeywords(query)}></input>
+			<input type='button' value='Refresh'
+				onClick={(e) => this.handleChangeInInput()}></input>
 		</form>
-	); 
-};
+	); 	
+  }
+}
