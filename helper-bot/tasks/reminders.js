ecff857a91ec9730eb98881ac7964936194d64ba
Sam Martin <samuelthomasmartin@gmail.com>
2018-01-18 11:33:29 -0800

    add basic functionality to reminders.js

--- helper-bot/tasks/reminders.js
+++ helper-bot/tasks/reminders.js
@@ -1,30 +1,64 @@
 const output = require('../../helper-bot/responder.js');
 
 
-const reminderMaker = (task, time, username, workspaceId) => {
-  let timeInterval = undefined;
-  let immediateBotMessageOnError = '';
-  let immediateBotMessageOnSuccess = '';
-  let timedBotMessageOnSuccess = '';
+const reminderMaker = (task, timeMessage, username, workspaceId) => {
+  let maxTimeOut = 604801; //seven days in seconds
+  let immediateBotMessageOnError;
   
-  //do logic to try to set timeInterval
+  let timeOut = timeCalculator(timeMessage);
 
-  if (timeInterval) {
+  if (timeOut < maxTimeOut) {
+	  let immediateBotMessageOnSuccess = `Got it, ${username}. I'm going to send you a reminder to ${task} ${timeMessage}`;
+	  let timedBotMessageOnSuccess = `Yo ${username}, this is a reminder to ${task}`;
+
+	  console.log(immediateBotMessageOnSuccess);
+
+  	setTimeout(() => {
+  		console.log(timedBotMessageOnSuccess)	
+  	}, timeOut * 1000);
+  } else if (timeInterval > maxTimeOut) {
   	//
-  } else {
+	} else {
 	  immediateBotMessageOnError = `Hey ${username}, I couldn't work out when you wanted me to remind you to ${task}. Please try again.`;
-	  console.log(immediateBotMessageOnError);
   	//output.responder('reminders', workspaceId, immediateBotMessageOnError, ws, wss);
   }
+};
+
+const timeCalculator = (timeMessage) => {
+	let timeInputs = timeMessage.split(' ');
+	let timeOut = 1;
+
+	timeInputs.forEach(input => {
+		for (let key in timeTable) {
+			if (input.indexOf(key) > -1) {
+				timeOut *= timeTable[key];
+			}
+		}
+
+		if (parseInt(input)) {
+			timeOut *= input;
+		}
+	});
+
+	return timeOut;
+};
+
+const timeTable = {
+	second: 1,
+	minute: 60,
+	hour: 60 * 60,
+	day: 60 * 60 * 24
 };
 
 
 module.exports = {reminderMaker};
 
 // VALID INPUT FORMATS
+// /helper-bot remind me to brush my teeth in 60 seconds
+// /helper-bot remind me to brush my teeth in 2 minutes
+// /helper-bot remind me to brush my teeth in 3 hours
+
 // /helper-bot remind me to take out the trash tomorrow at 5pm
 // /helper-bot remind me to pack later today
 // /helper-bot remind me to pack later next week
-// /helper-bot remind me to brush my teeth in 60 seconds
 // /helper-bot remind me to take out the trash on Sunday
-// /helper-bot remind aaaaahhhhhhh!
