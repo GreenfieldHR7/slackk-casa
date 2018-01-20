84deaef968c7beed3334f86b749515f2e31ac608
Sam Martin <samuelthomasmartin@gmail.com>
2018-01-19 19:58:55 -0800

    remove ref to config

--- helper-bot/tasks/meanings.js
+++ helper-bot/tasks/meanings.js
@@ -1,34 +1,34 @@
 const rp = require('request-promise');
 const TextRazor = require('textrazor');
 
-const config = require('../../config.js');
+//const config = require('../../config.js');
 const output = require('../../helper-bot/responder.js');
 
 
 const processTextWithNLP = (text, workspaceId, ws, wss) => {
-  let textRazor = new TextRazor(config.textRazerAPI_Token);
-  let content = text;
-  let options = { extractors: 'topics' }
+  // let textRazor = new TextRazor(config.textRazerAPI_Token);
+  // let content = text;
+  // let options = { extractors: 'topics' }
 
-  textRazor.exec(content, options)
-    .then(meaning => {
-    	let errorMessage = `Hmm. I didn't quite catch that. Unfortunately, I can only help with stuff like setting up reminders, creating to-do lists/ notes, and fetching the news.`;
+  // textRazor.exec(content, options)
+  //   .then(meaning => {
+  //   	let errorMessage = `Hmm. I didn't quite catch that. Unfortunately, I can only help with stuff like setting up reminders, creating to-do lists/ notes, and fetching the news.`;
 
-    	if (meaning.response.coarseTopics) {
-	    	let topics = [];
+  //   	if (meaning.response.coarseTopics) {
+	 //    	let topics = [];
     		
-	    	meaning.response.coarseTopics.forEach(topic => {
-	    		topics.push(topic.label.toLowerCase());
-	    	});
+	 //    	meaning.response.coarseTopics.forEach(topic => {
+	 //    		topics.push(topic.label.toLowerCase());
+	 //    	});
 
-		    errorMessage = `Hmm. It looks like you are looking for help with ${topics[0]} or ${topics[1]}. Unfortunately, I can only help with stuff like setting up reminders, creating to-do lists/ notes, and fetching the news.`;
-    	}
+		//     errorMessage = `Hmm. It looks like you are looking for help with ${topics[0]} or ${topics[1]}. Unfortunately, I can only help with stuff like setting up reminders, creating to-do lists/ notes, and fetching the news.`;
+  //   	}
 
-	    output.responder(workspaceId, errorMessage, ws, wss);
-    }) 
-    .catch(err => {
-    	console.error(err);
-    })
+	 //    output.responder(workspaceId, errorMessage, ws, wss);
+  //   }) 
+  //   .catch(err => {
+  //   	console.error(err);
+  //   })
 }
 
 
